4438093a8aa560681306bbae8ce9162856c562f2
Pablo casa padres <pablohnavarrete@gmail.com>
2021-04-14 17:36:47 -0300

    finalizado

--- tp1_backend.php
+++ tp1_backend.php
@@ -43,21 +43,65 @@
       ?>
     </h3>
 <hr>
-    <h1> EJERCICIO 2 </h1>
+    <h1> EJERCICIO 4 </h1>
       <h2 style="color:GREEN">
+        TEMPERATURA Celsius:<br>
       <?php
-      $algoparaimprimir="Hola mundo";
-      echo "$algoparaimprimir";
+      $tempcel=20;
+
+      echo "$tempcel<br>";
+      ?>
+
+      TEMPERATURA Fahrenheit:<br>
+
+      <?php
+      $tempfar=($tempcel*9/5)+32;
+      echo "$tempfar<br>";
       ?>
       </h2>
 <hr>
-    <h1> EJERCICIO 2 </h1>
-      <h2 style="color:RED">
+    <h1> EJERCICIO 5 </h1>
+    <h2> INCISO A </h2>
+    <h3 style="color:red">
+      DATOS del rectangulo:<br>
+    Base:
+    <?php
+    $B=18;
+    echo "$B<br>";
+    ?>
+    Altura:
       <?php
-      $algoparaimprimir="Hola mundo";
-      echo "$algoparaimprimir";
+      $H=12;
+      echo "$H<br>";
       ?>
-    </h2>
+
+    AREA Y PERIMETRO:<br>
+
+    <?php
+    $AREA=$H*$B;
+    echo "$AREA","cm^2<br>";
+    $PER=2*($H+$B);
+    echo "$PER","cm<br>";
+    ?>
+    </h3>
+    <h2> INCISO B </h2>
+    <h3 style="color:red">
+      DATOS del circulo:<br>
+      Diametro:
+    <?php
+    $D=20;
+    echo "$D<br>";
+    ?>
+
+    AREA Y PERIMETRO:<br>
+
+    <?php
+    $AREACIR=PI()*($D/2)^2;
+    echo "$AREACIR","cm^2<br>";
+    $PERCIR=PI()*$D;
+    echo "$PERCIR","cm<br>";
+    ?>
+    </h3>
 <hr>
   </body>
 </html>
