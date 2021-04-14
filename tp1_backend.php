62eb84c2a205428b473516ffa40246b2a7ca1f9e
Pablo casa padres <pablohnavarrete@gmail.com>
2021-04-14 17:11:38 -0300

    priemros ejercicios

---
+++ tp1_backend.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>TRABAJO PRACTICO Nª1</title>
+  </head>
+  <body>
+
+    <h1> EJERCICIO 1 </h1>
+      <h2 style="color:ORANGE">
+      Hola mundo
+      </h2>
+<hr>
+    <h1> EJERCICIO 2 </h1>
+      <h2 style="color:BROWN">
+      <?php
+      $algoparaimprimir="Hola mundo";
+      echo "$algoparaimprimir";
+      ?>
+    </h2>
+<hr>
+    <h1> EJERCICIO 3 </h1>
+      <h2 style="color:BLUE">
+      VARIABLES:<br>
+      <?php
+      $VARINT1=5;
+      $VARINT2=2;
+      echo "$VARINT1<br>";
+      echo "$VARINT2<br>";
+      ?>
+      OPERACIONES:<br>
+      <?php
+      $SUMA=$VARINT1+$VARINT2;
+      $RESTA=$VARINT1-$VARINT2;
+      $MULTIPLICACION=$VARINT1*$VARINT2;
+      $DIVISION=$VARINT1/$VARINT2;
+      $DIVISIONRESTO=$VARINT1%$VARINT2;
+      echo "$SUMA<br>";
+      echo "$RESTA<br>";
+      echo "$MULTIPLICACION<br>";
+      echo "$DIVISION<br>";
+      echo "$DIVISIONRESTO<br>";
+      ?>
+    </h3>
+<hr>
+    <h1> EJERCICIO 2 </h1>
+      <h2 style="color:GREEN">
+      <?php
+      $algoparaimprimir="Hola mundo";
+      echo "$algoparaimprimir";
+      ?>
+      </h2>
+<hr>
+    <h1> EJERCICIO 2 </h1>
+      <h2 style="color:RED">
+      <?php
+      $algoparaimprimir="Hola mundo";
+      echo "$algoparaimprimir";
+      ?>
+    </h2>
+<hr>
+  </body>
+</html>
